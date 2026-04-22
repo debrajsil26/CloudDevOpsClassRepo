b488b9465a698bb158079e27e4376b88610ea5ad
Debraj sil <debrajsil@gmail.com>
2026-04-23 02:14:20 +0530

    Update greeting message in HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 //Write a hello world program in java
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World Debraj!");
     }
 }
