ff8e799b539a6fd1b157a78731ccbd2865d676fc
Debraj sil <debrajsil@gmail.com>
2026-04-24 00:00:54 +0530

    Rename class and update hello world message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 //Write a hello world program in java
-public class Main {
+public class HelloWorldPrint {
     public static void main(String[] args) {
         System.out.println("Hello, World Debraj!");
     }
